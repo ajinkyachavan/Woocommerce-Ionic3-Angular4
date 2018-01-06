bc08afc5d827062e11068f2031fa1b8cdee3579e
ajinkyachavan <a.ajinkya9030@gmail.com>
2018-01-06 02:56:15 -0500

    Committed my state

--- README.md
+++
@@ -1,2 +0,0 @@
-# Woocommerce-Ionic3-Angular4
-An ecommerce mobile app using Ionic 3 and Angular 4 based on Woocommerce API
---
+++ git_init.sh
@@ -0,0 +1,13 @@
+git init
+git add -A
+git commit -m "my latest"
+
+git remote add origin https://github.com/ajinkyachavan/Woocommerce-Ionic3-Angular4
+git fetch origin
+
+git reset origin/master
+
+git add -A
+git commit -m "Committed my state"
+
+git push -u origin master
